005172ee1680f5ee2e029bf03688ead9fb61a484
amaraa <learn@1234.mn>
2019-12-03 12:43:59 +0800

    Finance module ruu item add hiiideg bolgono

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var uiController = (function() {
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
@@ -24,20 +24,23 @@ var uiController = (function() {
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
+  // private data
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private data
   var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: []
     },
@@ -47,14 +50,40 @@ var financeController = (function() {
       exp: 0
     }
   };
+
+  return {
+    addItem: function(type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+
+    seeData: function() {
+      return data;
+    }
+  };
 })();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
   var ctrlAddItem = function() {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
+
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
     // 4. Төсвийг тооцоолно
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
